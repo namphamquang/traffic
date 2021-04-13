ce8a30917a7ae23baae4cf677ccf27315fbab22c
namphamquang <80265358+namphamquang@users.noreply.github.com>
2021-04-14 00:56:12 +0700

    Add files via upload

---
+++ common.h
@@ -0,0 +1,78 @@
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+#define SDL_ERROR 1
+#define IMG_ERROR 2
+#define MIX_ERROR 3
+#define TTF_ERROR 4
+
+#define MIX_CHANNEL -1
+#define TIME_MAX 1000
+
+#define SPEED_INCREASEMENT 2
+#define SCORE_INCREASEMENT 1
+#define TIME_INCREASEMENT 1
+#define FRAME_INCREASEMENT 1
+
+#define BACKGROUND_SPEED 5
+#define OBS_SPEED 5
+
+#define IS_REPEATITIVE -1
+#define NOT_REPEATITIVE 0
+
+#define SMALL_BUTTON 1
+#define COMMON_BUTTON 2
+
+const std::string WINDOW_TITLE = "Traffic";
+
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 600;
+
+const int COMMON_BUTTON_WIDTH = 75;
+const int COMMON_BUTTON_HEIGHT = 49;
+const int SMALL_BUTTON_WIDTH = 22;
+const int SMALL_BUTTON_HEIGHT = 34;
+
+const int PLAY_BUTON_POSX = 160;
+const int PLAY_BUTTON_POSY= 300;
+const int HELP_BUTTON_POSX = 160;
+const int HELP_BUTTON_POSY = 400;
+const int EXIT_BUTTON_POSX = 160;
+const int EXIT_BUTTON_POSY = 500;
+const int BACK_BUTTON_POSX = 31;
+const int BACK_BUTTON_POSY = 29;
+const int PAUSE_BUTTON_POSX = 31;
+const int PAUSE_BUTTON_POSY = 29;
+const int CONTINUE_BUTTON_POSX = 31;
+const int CONTINUE_BUTTON_POSY = 29;
+
+const int TEXT_1_POSX = 260;
+const int TEXT_1_POSY = 20;
+const int TEXT_2_POSX = 260;
+const int TEXT_2_POSY = 60;
+const int SCORE_POSX = 350;
+const int SCORE_POSY = 20;
+const int HIGH_SCORE_POSX = 350;
+const int HIGH_SCORE_POSY = 60;
+
+enum ButtonSprite
+{
+	BUTTON_MOUSE_OUT = 0,
+	BUTTON_MOUSE_OVER = 1,
+	BUTTON_TOTAL = 2
+};
+
+void LogError(std::string msg, int error_code = SDL_ERROR);
+
+#endif // COMMON_H_INCLUDED
---
+++ obstacle.cpp
@@ -0,0 +1,98 @@
+#include "obstacle.h"
+
+Obstacle::Obstacle() //khởi tạo vật cản
+{
+	posX1 = 0;
+	posY1 = 0;
+	eWidth = 0;
+	eHeight = 0;
+	ObsTexture = nullptr;
+}
+
+Obstacle::~Obstacle() //giải phóng vật cản
+{
+	posX1 = 0;
+	posY1 = 0;
+	eWidth = 0;
+	eHeight = 0;
+	if (ObsTexture != nullptr)
+        {
+            ObsTexture = nullptr;
+        }
+}
+
+void Obstacle::LoadFromFile(std::string path, SDL_Renderer* gRenderer) //tải ảnh của vật cản
+{
+	SDL_Texture* tmpTexture = nullptr;
+
+	SDL_Surface* tmpSurface = IMG_Load(path.c_str());
+	if (tmpSurface == nullptr)
+	{
+		LogError("Can not load image.", IMG_ERROR);
+	}
+	else
+	{
+		SDL_SetColorKey(tmpSurface, SDL_TRUE, SDL_MapRGB(tmpSurface->format, 0, 255, 255));
+
+		tmpTexture = SDL_CreateTextureFromSurface(gRenderer, tmpSurface);
+		if (tmpTexture == nullptr)
+		{
+			LogError("Can not create texture from surface.", SDL_ERROR);
+		}
+		else
+		{
+			eWidth = tmpSurface->w;
+			eHeight = tmpSurface->h;
+		}
+
+		SDL_FreeSurface(tmpSurface);
+	}
+
+	ObsTexture = tmpTexture;
+}
+
+void Obstacle::Move(const int &acceleration) //di chuyển của vật cản với gia tốc
+{
+	posY1 += OBS_SPEED + acceleration;
+	if (posY1  > SCREEN_HEIGHT)
+	{
+		posY1=-60;
+        posX1 = 90 + (rand()%4)*60 ;
+	}
+}
+
+void Obstacle::Render(SDL_Renderer* gRenderer, SDL_Rect* currentClip) //vẽ vật cản
+{
+	SDL_Rect renderSpace = { posX1, posY1, eWidth, eHeight };
+	if (currentClip != nullptr)
+	{
+		renderSpace.w = currentClip->w;
+		renderSpace.h = currentClip->h;
+	}
+	SDL_RenderCopy(gRenderer, ObsTexture, currentClip, &renderSpace);
+}
+
+int Obstacle::GetSpeed(const int &acceleration) //cập nhật tốc độ
+{
+	return OBS_SPEED + acceleration;
+}
+
+int Obstacle::GetPosX() //vị trí x
+{
+	return posX1;
+}
+
+int Obstacle::GetPosY() //vị trí y
+{
+	return posY1;
+}
+
+int Obstacle::GetWidth() //chiều rộng
+{
+	return eWidth;
+}
+
+int Obstacle::GetHeight() //chiều dài
+{
+	return eHeight;
+}
